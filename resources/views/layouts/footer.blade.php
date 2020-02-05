6d0d73a29e29cb94212b7d9a2120c76165f4b349
andres <ajuanenama@zubirimanteo.com>
2020-02-05 19:13:33 +0100

    punto uno del ejercicio

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,15 @@
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Desarrollo web en entorno servidor 2020</p>
+    </div>
+    <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
